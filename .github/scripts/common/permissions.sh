a291dca2aa7565846e0fc26acb809db4a2a8418d
dvun0g <nikita.runikita.ru@gmail.com>
2023-03-23 12:36:03 +0300

    fix(Frontend-2): тестовый коммит, хочу посмотреть есть ли env переменные в скрипте.

--- .github/scripts/common/permissions.sh
+++ .github/scripts/common/permissions.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 echo "Permissions ci script started..."
+echo "$IS_FRONTEND $PATH_BUILD_SCRIPT $PATH_LINTERS_SCRIPT $PATH_INSTALL_DEPS_SCRIPT"
 if [ $IS_FRONTEND -e 1 ]; then
     echo "Is Frontend."
     chmod +x $PATH_BUILD_SCRIPT

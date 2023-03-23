971ae87cb1f1e5122cc04afd4828b22c4a8725a8
dvun0g <nikita.runikita.ru@gmail.com>
2023-03-23 12:32:31 +0300

    fix(Frontend-2): поправил скрипт в ci / cd.

--- .github/scripts/common/permissions.sh
+++ .github/scripts/common/permissions.sh
@@ -3,7 +3,12 @@
 echo "Permissions ci script started..."
 if [ $IS_FRONTEND -e 1 ]; then
     echo "Is Frontend."
-    chmod +x PATH_BUILD_SCRIPT
-    chmod +x PATH_LINTERS_SCRIPT
-    chmod +x PATH_INSTALL_DEPS_SCRIPT
+    chmod +x $PATH_BUILD_SCRIPT
+    chmod +x $PATH_LINTERS_SCRIPT
+    chmod +x $PATH_INSTALL_DEPS_SCRIPT
+
+    if [ $? -e 0 ]; then
+        echo "The script ended successfully."
+    fi
+    exit 0
 fi
